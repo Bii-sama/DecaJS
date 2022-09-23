27e787cf6f15f034b887cfe02a85e066bd46a71a
Abidemi Omosanya <mail.bidemi75@gmail.com>
2022-09-23 06:53:18 -1200

    add

--- decajs.js
+++ decajs.js
@@ -1,13 +1,14 @@
-let i = 2
+/*let i = 2
 while(1 < 10){
   console.log(i)
   i+2
-}
+}*/
 
 
 let userHat = ['great','house','is','by','the','river']
 
-for(let x = 0; x < userHat.length; x++){
-    console.log(userHat[x])
-    x.toUpperCase();
+for(let x = 0; x <= userHat.length; x++){
+  
+    console.log(userHat[x].toUpperCase())
+   
 }
